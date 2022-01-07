fac04ce5ac4b3fff05c6c88b5892937647939129
Antonnn95 <34644124+Antonnn95@users.noreply.github.com>
2022-01-07 11:39:53 +0100

    Add repo

---
+++ CalculatorAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("add")]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [HttpGet("sub")]
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        [HttpGet("mult")]
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        [HttpGet("div")]
+        public double Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
